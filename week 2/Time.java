f76a5684492c58fff80968776b01e8fda1571e6d
Madison Nguyen <83198061+madisonngyn@users.noreply.github.com>
2021-11-12 22:27:14 -0800

    Update Time.java

--- week 2/Time.java
+++ week 2/Time.java
@@ -1,30 +1,31 @@
-class Time {
+public class Time {
+    
     public static void main(String[] args) {
-    float hour = 8;                 // time when I started coding
+    
+    // time when I started coding
+    float hour = 8;                 
     float minute = 27; 
     float second = 48;
     
-    System.out.print("Number of seconds since midnight: ");
+    // finding elapsed time (in seconds) from midnight
     float secondsElapsed = second + (minute * 60) + (hour * 3600);
-    System.out.println(secondsElapsed);
+    System.out.println("Number of seconds since midnight: " + secondsElapsed);
     
-    System.out.print("Number of seconds remaining in the day: ");
+    // finding seconds remaining in the day
     float secondsRemaining = ((60*60*24) - (secondsElapsed));
-    System.out.println(secondsRemaining);
+    System.out.println("Number of seconds remaining in the day: " + secondsRemaining);
     
-    System.out.print("Percent of the day that has passed: ");
+    // finding percent of day that has passed
     float percentageLeft = ((secondsElapsed / 86400) * 100);
-    System.out.print(percentageLeft);
-     System.out.println("%");
-   
+    System.out.println("Percent of the day that has passed: " + percentageLeft + "%");
     
-    float endHour = 10;              // time when I finished coding
+    // time when I finished coding
+    float endHour = 10;         
     float endMinute = 28;
     float endSecond = 54;
     
-    System.out.print("Elapsed time since I started working on this exercise: ");
-    
+    // finding elapsed time since I started working on exercise
     float elapsedTime = ((endSecond + (endMinute * 60) + (endHour * 3600)) - (secondsElapsed));
-    System.out.println(elapsedTime);
+    System.out.println("Elapsed time since I started working on this exercise: " + elapsedTime);
     }
 }
