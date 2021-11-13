b4e332a229479ec4d84d19279eb432a72982acce
Madison Nguyen <83198061+madisonngyn@users.noreply.github.com>
2021-11-12 21:58:43 -0800

    Add files via upload

---
+++ week 3/Staircase.java
@@ -0,0 +1,41 @@
+// Madison Nguyen
+// September 16th, 2021
+// This program is used the calculate the number of cubic centimeters of concrete needed to build a staircase by using input of the staircase's rise, run, and width from the user.
+
+import java.util.Scanner;
+
+public class Staircase {
+	
+	public static void main (String[] args) {
+		Scanner in = new Scanner(System.in);
+		
+		System.out.println("Staircase Volume Calculator");
+		
+		System.out.print("How many steps in the staircase? ");
+		int numberOfSteps = in.nextInt();
+		
+		// calculating the number of blocks needed for the staircase
+		int sumBlocks = (((numberOfSteps) * ((numberOfSteps) + 1)) / 2);
+	
+		// calculating the volume of each block
+		System.out.print("Enter step width in cm: ");
+		double stepWidth = in.nextDouble();
+		
+		System.out.print("Enter step rise in cm: ");
+		double stepRise = in.nextDouble();
+		
+		System.out.print("Enter step run in cm: ");
+		double stepRun = in.nextDouble();
+		
+		double volumeOfBlock = (stepWidth * stepRise * stepRun);
+		
+		// calclating the total volume of the blocks
+		double totalVolumeOfBlocks = (sumBlocks * (stepWidth * stepRise * stepRun));
+		int totalVolumeOfBlocksRounded = (int) (totalVolumeOfBlocks + 0.5);
+		System.out.print("Total volume is ");
+		System.out.print(totalVolumeOfBlocksRounded);	
+		System.out.print(" cubic centimeters");
+		
+	}
+}
+
